0d743afded4beeb9df2ea26d09b8243560c4f55b
NilayGanvit <79703214+NilayGanvit@users.noreply.github.com>
2022-05-29 22:39:18 +0530

    Update resell-nft.js

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -54,7 +54,7 @@ export default function ResellNFT() {
         />
         {
           image && (
-            <img className="rounded mt-4" width="350" src={image} />
+            
           )
         }
         <button onClick={listNFTForSale} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
